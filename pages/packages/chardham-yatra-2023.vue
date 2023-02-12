075a18041fdce25e4ceac15c2b290190d2761cfd
Faisal <ifaisalabid1>
2023-02-12 11:01:26 +0530

    Add form

--- pages/packages/chardham-yatra-2023.vue
+++ pages/packages/chardham-yatra-2023.vue
@@ -2,100 +2,67 @@
   <div class="my-5">
     <!-- hero -->
     <section class="chardham_hero d-flex align-items-center">
-      <div class="container text-capitalize display-4 text-center">
-        <p class="text-white">
-          book hassle free chardham yatra 2023 with UCS Cab
-        </p>
-        <a
-          href="tel:+919520801801"
-          class="btn btn-rounded fs-6"
-          target="_blank"
-          role="button"
-        >
-          <i class="fa-solid fa-phone me-1 text-white"></i> +91 95208 01801
-        </a>
-      </div>
-    </section>
-
-    <!-- rates -->
-    <section class="my-5 py-md-5 container">
-      <h3 class="fw-bold">Taxi Fare for Chardham Yatra 2023:</h3>
-      <div class="table-responsive">
-        <table class="table table-striped table-hover mt-4">
-          <thead>
-            <tr>
-              <th scope="col" class="fw-bold fs-5 text-white">Cab / Taxi</th>
-              <th scope="col" class="fw-bold fs-5 text-white">Tariff</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">Sedan</th>
-              <td>₹ 4500/- Per Day</td>
-            </tr>
-            <tr>
-              <th scope="row">Mini SUV</th>
-              <td>₹ 5500/ - Per Day</td>
-            </tr>
-            <tr>
-              <th scope="row">SUV</th>
-              <td>₹ 6500/- Per Day</td>
-            </tr>
-            <tr>
-              <th scope="row">Premium SUV</th>
-              <td>₹ 7000/- Per Day</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="text-center mt-5">
-        <a
-          href="tel:+919520801801"
-          class="btn btn-rounded fs-5"
-          target="_blank"
-          role="button"
-        >
-          <i class="fa-solid fa-phone me-1 text-white"></i> book now with UCS
-          Cab
-        </a>
-      </div>
-    </section>
-
-    <!-- tour content -->
-    <section class="py-5 tour_content">
-      <div class="container py-5">
-        <div class="row g-5">
-          <div class="col-md-6 my-auto">
-            <h3 class="fw-bold">
-              Make the Most of your Chardham Yatra Tour with UCS Cab
-            </h3>
-            <p class="mt-4">
-              Planning for your Chardham yatra this year? You have knocked on
-              the right door. UCS Cab will ensure that your tour of four holy
-              sites is seamless. Among the countless religious sites and
-              circuits that devotees visit in Uttarakhand, one of the most
-              prominent is the Uttarakhand Chardham yatra.
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 my-auto d-none d-md-block">
+            <p class="text-white display-6 text-capitalize">
+              book hassle free chardham yatra 2023 with UCS Cab
             </p>
+            <a
+              href="tel:+919520801801"
+              class="btn btn-rounded fs-6"
+              target="_blank"
+              role="button"
+            >
+              <i class="fa-solid fa-phone me-1 text-white"></i> +91 95208 01801
+            </a>
           </div>
-          <div class="col-md-6">
-            <nuxt-img preset="cover"
-              src="/kedarnath.jpg"
-              alt="Mountains"
-              class="img-fluid rounded"
-            />
+          <div class="col-md-6 py-5">
+            <form class="bg-white rounded-7 p-5">
+              <h3 class="text-dark text-center fw-bold fs-1">
+                Book Your Cab Now
+              </h3>
+              <div class="mb-md-3">
+                <label for="name" class="form-label"></label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="name"
+                  placeholder="Name"
+                />
+              </div>
+              <div class="mb-md-3">
+                <label for="email" class="form-label"></label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email"
+                  placeholder="Email Address"
+                />
+              </div>
+              <div class="mb-md-3">
+                <label for="ph-number" class="form-label"></label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="ph-number"
+                  placeholder="Mobile Number"
+                />
+              </div>
+              <div class="mb-md-3">
+                <label for="date" class="form-label"></label>
+                <input type="date" class="form-control" id="date" />
+              </div>
+
+              <button
+                type="submit"
+                class="btn btn-primary d-block mx-auto btn-rounded fs-6 mt-4"
+              >
+                Send Your Query
+              </button>
+            </form>
           </div>
         </div>
-      </div>
-      <div class="text-center mt-4">
-        <a
-          href="tel:+919520801801"
-          class="btn btn-rounded fs-5"
-          target="_blank"
-          role="button"
-        >
-          <i class="fa-solid fa-phone me-1 text-white"></i> book now with UCS
-          Cab
-        </a>
       </div>
     </section>
 
@@ -147,6 +114,89 @@
         </a>
       </div>
     </section>
+
+    <!-- tour content -->
+    <section class="py-5 tour_content">
+      <div class="container py-5">
+        <div class="row g-5">
+          <div class="col-md-6 my-auto">
+            <h3 class="fw-bold">
+              Make the Most of your Chardham Yatra Tour with UCS Cab
+            </h3>
+            <p class="mt-4">
+              Planning for your Chardham yatra this year? You have knocked on
+              the right door. UCS Cab will ensure that your tour of four holy
+              sites is seamless. Among the countless religious sites and
+              circuits that devotees visit in Uttarakhand, one of the most
+              prominent is the Uttarakhand Chardham yatra.
+            </p>
+          </div>
+          <div class="col-md-6">
+            <nuxt-img
+              preset="cover"
+              src="/kedarnath.jpg"
+              alt="Mountains"
+              class="img-fluid rounded"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="text-center mt-4">
+        <a
+          href="tel:+919520801801"
+          class="btn btn-rounded fs-5"
+          target="_blank"
+          role="button"
+        >
+          <i class="fa-solid fa-phone me-1 text-white"></i> book now with UCS
+          Cab
+        </a>
+      </div>
+    </section>
+
+    <!-- rates -->
+    <section class="my-5 py-md-5 container">
+      <h3 class="fw-bold">Taxi Fare for Chardham Yatra 2023:</h3>
+      <div class="table-responsive">
+        <table class="table table-striped table-hover mt-4">
+          <thead>
+            <tr>
+              <th scope="col" class="fw-bold fs-5 text-white">Cab / Taxi</th>
+              <th scope="col" class="fw-bold fs-5 text-white">Tariff</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Sedan</th>
+              <td>₹ 4500/- Per Day</td>
+            </tr>
+            <tr>
+              <th scope="row">Mini SUV</th>
+              <td>₹ 5500/ - Per Day</td>
+            </tr>
+            <tr>
+              <th scope="row">SUV</th>
+              <td>₹ 6500/- Per Day</td>
+            </tr>
+            <tr>
+              <th scope="row">Premium SUV</th>
+              <td>₹ 7000/- Per Day</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="text-center mt-5">
+        <a
+          href="tel:+919520801801"
+          class="btn btn-rounded fs-5"
+          target="_blank"
+          role="button"
+        >
+          <i class="fa-solid fa-phone me-1 text-white"></i> book now with UCS
+          Cab
+        </a>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -177,7 +227,7 @@ thead {
 
 @media screen and (max-width: 768px) {
   .chardham_hero {
-    height: 300px;
+    height: auto;
   }
 }
 </style>
